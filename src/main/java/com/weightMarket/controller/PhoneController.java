94029dc2286a56a11d14dc3ac32a9d386e76c677
handongqignxue <1417140290@qq.com>
2021-03-12 11:07:35 +0800

    first commit

---
+++ src/main/java/com/weightMarket/controller/PhoneController.java
@@ -0,0 +1,11 @@
+package com.weightMarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/phone")
+public class PhoneController {
+	
+}
+
