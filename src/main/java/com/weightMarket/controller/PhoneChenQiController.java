3e132a14750fcf809d0234be3393c8426ad54207
handongqignxue <1417140290@qq.com>
2021-04-07 10:55:50 +0800

    完善

--- src/main/java/com/weightMarket/controller/PhoneChenQiController.java
+++ src/main/java/com/weightMarket/controller/PhoneChenQiController.java
@@ -1,9 +1,7 @@
 package com.weightMarket.controller;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,8 +12,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.weightMarket.entity.*;
 import com.weightMarket.service.*;
-import com.weightMarket.util.JsonUtil;
-import com.weightMarket.util.PlanResult;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -47,7 +43,7 @@ public class PhoneChenQiController {
 			else {
 		        jsonpCallback="jsonpCallback(\"{\\\"status\\\":1,\\\"msg\\\":\\\"获取报价成功，请等待客服电话回复！\\\"}\")";
 			}
-			response.getWriter().print(jsonpCallback);
+			ajaxResponse(response,jsonpCallback);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -69,12 +65,7 @@ public class PhoneChenQiController {
 	        jsonpCallback="jsonpCallback(\"{\\\"message\\\":\\\"ok\\\",\\\"data\\\":"+gpuListStr+"}\")";
 		}
 
-        try {
-			response.getWriter().print(jsonpCallback);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		ajaxResponse(response,jsonpCallback);
 	}
 
 	@RequestMapping(value="/selectProdNeedList")
@@ -92,12 +83,7 @@ public class PhoneChenQiController {
 	        jsonpCallback="jsonpCallback(\"{\\\"message\\\":\\\"ok\\\",\\\"data\\\":"+pnListStr+"}\")";
 		}
 
-        try {
-			response.getWriter().print(jsonpCallback);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		ajaxResponse(response,jsonpCallback);
 	}
 
 	@RequestMapping(value="/selectExampleShowList")
@@ -115,12 +101,7 @@ public class PhoneChenQiController {
 	        jsonpCallback="jsonpCallback(\"{\\\"message\\\":\\\"ok\\\",\\\"data\\\":"+esListStr+"}\")";
 		}
 
-        try {
-			response.getWriter().print(jsonpCallback);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		ajaxResponse(response,jsonpCallback);
 	}
 
 	@RequestMapping(value="/getSystemInfo")
@@ -139,12 +120,7 @@ public class PhoneChenQiController {
 	        jsonpCallback="jsonpCallback(\"{\\\"message\\\":\\\"ok\\\",\\\"systemInfo\\\":"+siStr+"}\")";
 		}
 
-        try {
-			response.getWriter().print(jsonpCallback);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		ajaxResponse(response,jsonpCallback);
 	}
 
 	@RequestMapping(value="/getSecretPolicy")
@@ -154,7 +130,16 @@ public class PhoneChenQiController {
 		String yszc = systemInfoService.get().getYszc().replaceAll("\"", "\\\\\\\\\\\\\"");
 		String jsonpCallback="jsonpCallback(\"{\\\"yszc\\\":\\\""+yszc+"\\\"}\")";
 
-        try {
+		ajaxResponse(response,jsonpCallback);
+	}
+	
+	/**
+	 * 调用ajax返回
+	 * @param response
+	 * @param jsonpCallback
+	 */
+	public void ajaxResponse(HttpServletResponse response,String jsonpCallback) {
+		try {
 			response.getWriter().print(jsonpCallback);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
